e2a1849b0c583561303041f02e0d168521ee7dcf
RunAtTekky <varunrawat343@gmail.com>
2025-08-31 23:43:29 +0530

    feat: We are now comparing actual structs parsed by JSON
    format json
    
    map is compared key by key

--- helper/input_test.go
+++ helper/input_test.go
@@ -8,33 +8,51 @@ import (
 
 func TestInput(t *testing.T) {
 	want := `{
-"keys": {
-"n": 4,
-"k": 3
-},
-"1": {
-"base": "10",
-"value": "4"
-},
-"2": {
-"base": "2",
-"value": "111"
-},
-"3": {
-"base": "10",
-"value": "12"
-},
-"6": {
-"base": "4",
-"value": "213"
-}
+    "keys": {
+        "n": 4,
+        "k": 3
+    },
+    "1": {
+        "base": "10",
+        "value": "4"
+    },
+    "2": {
+        "base": "2",
+        "value": "111"
+    },
+    "3": {
+        "base": "10",
+        "value": "12"
+    },
+    "6": {
+        "base": "4",
+        "value": "213"
+    }
 }`
-	wantRawData, _, _ := model.Json_parser(want)
+	_, wantKey, wantEntries := model.Json_parser(want)
 
 	got := TakeInput("..")
-	gotRawData, _, _ := model.Json_parser(got)
+	_, gotKey, gotEntries := model.Json_parser(got)
 
-	if !reflect.DeepEqual(wantRawData, gotRawData) {
-		t.Errorf("got \n%v\nBut want\n%v", got, want)
+	if !reflect.DeepEqual(gotKey, wantKey) {
+		t.Errorf("got \n%v\nBut want\n%v", gotKey, wantKey)
 	}
+
+	if !SameEntries(gotEntries, wantEntries) {
+		t.Errorf("got \n%v\nBut want\n%v", gotEntries, wantEntries)
+	}
+}
+
+func SameEntries(mapA, mapB map[string]model.Entry) bool {
+	if len(mapA) != len(mapB) {
+		return false
+	}
+	for key, val := range mapA {
+		valB := mapB[key]
+
+		if val != valB {
+			return false
+		}
+	}
+	return true
 }
